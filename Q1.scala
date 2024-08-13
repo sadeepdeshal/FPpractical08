30b4d92d5cdeebb6534a639b3e2bb26abd9c242d
sadeepdeshal <140165441+sadeepdeshal@users.noreply.github.com>
2024-08-13 05:42:25 +0530

    Add files via upload

---
+++ Q1.scala
@@ -0,0 +1,41 @@
+object  Q1{
+
+  val alphabet = 'A' to 'Z'
+
+  def encrypt(shift: Int, text: String): String = {
+    text.map { char =>
+      if (char.isLetter) {
+        val shiftedChar = alphabet((alphabet.indexOf(char.toUpper) + shift) % alphabet.size)
+        if (char.isLower) shiftedChar.toLower else shiftedChar
+      } else {
+        char
+      }
+    }
+  }
+
+  def decrypt(shift: Int, text: String): String = {
+    text.map { char =>
+      if (char.isLetter) {
+        val shiftedChar = alphabet((alphabet.indexOf(char.toUpper) - shift + alphabet.size) % alphabet.size)
+        if (char.isLower) shiftedChar.toLower else shiftedChar
+      } else {
+        char
+      }
+    }
+  }
+
+  def cipher(func: (Int, String) => String, shift: Int, text: String): String = {
+    func(shift, text)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val plaintext = "Hello, World!"
+    val shift = 3
+
+    val encryptedText = cipher(encrypt, shift, plaintext)
+    println(s"Encrypted: $encryptedText") 
+
+    val decryptedText = cipher(decrypt, shift, encryptedText)
+    println(s"Decrypted: $decryptedText")
+  }
+}
---
+++ Q2.scala
@@ -0,0 +1,20 @@
+import scala.io.StdIn
+
+object Q2 {
+
+  def main(args: Array[String]): Unit = {
+    println("Enter the number which you want to categorize: ")
+    val number = StdIn.readLine().toInt
+
+    def categorize(n: Int): String = n match {
+      case _ if n % 3 == 0 && n % 5 == 0 => "multiple of three and five"
+      case _ if n % 3 == 0 => "multiple of three"
+      case _ if n % 5 == 0 => "multiple of five"
+      case _ => "not a multiple of three or five"
+    }
+
+    val result = categorize(number)
+    println(result)
+  }
+}
+
